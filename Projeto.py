dd541f9f367f4a3fc79a55a8cb5445a7c10e721d
Adryhanmiguell <migueladryhan202@gmail.com>
2025-06-15 19:11:05 -0300

    Add files via upload

--- Projeto.py
+++ Projeto.py
@@ -9,25 +9,14 @@ import cadastros.reserva
 import cancelar_remover.cancelar_reserva
 import cancelar_remover.remover_carona
 import login.fazer_login
+import login.abrir_usuarios
 import mostrar.buscar_caronas
 import mostrar.carona_cadastrada
 import mostrar.detalhes_carona
 import mostrar.listar_caronas
 import mostrar.relatorio
 
-if os.path.exists('usuarios.txt'):
-    with open('usuarios_salvos.txt', 'r', encoding = 'utf-8') as arquivo:
-        for i in arquivo:
-            ler_linhas = i.strip().split(';')
-            if len(ler_linhas) == 3:
-                nome, email, senha = ler_linhas
-                usuarios.append({
-                    'nome': nome,
-                    'email': email,
-                    'senha': senha
-                })
-else:
-    pass
+login.abrir_usuarios.abrir_arquivo(usuarios)
 
 os.system('cls' if os.name == 'nt' else 'clear')
 
@@ -48,10 +37,15 @@ while True:
         cadastros.usuario.cadastrar_usuario(usuarios)
 
     elif opcao == '2':
-        usuario_logado = login.fazer_login.logar(usuarios)
+        usuario = login.fazer_login.logar(usuarios)
+        
+        if not usuario:
+            continue
+
         os.system('cls' if os.name == 'nt' else 'clear')
                 
         while True:
+
             print()
             print('-'*50)
             print('FUNÇÕES'.center(50))
@@ -73,31 +67,31 @@ while True:
             opcao2 = input("\nEscolha uma das opções: ")
         
             if opcao2 == '1':
-                caronas = cadastros.carona.cadastrar_carona(usuario_logado, caronas)
+                caronas = cadastros.carona.cadastrar_carona(usuario, caronas)
                 
             elif opcao2 == '2':
-                mostrar.listar_caronas.listar(caronas, usuario_logado)
+                mostrar.listar_caronas.listar(caronas, usuario)
                                                             
             elif opcao2 == '3':
                 mostrar.buscar_caronas.buscar(caronas)
                     
             elif opcao2 == '4':
-                passageiros = cadastros.reserva.reservar(caronas, usuarios, usuario_logado)
+                passageiros = cadastros.reserva.reservar(caronas, usuarios, usuario)
 
             elif opcao2 == '5': 
                 cancelar_remover.cancelar_reserva.remover_reserva(caronas, usuarios)
                     
             elif opcao2 == '6':
-                cancelar_remover.remover_carona.remover_carona(caronas, usuario_logado)
+                cancelar_remover.remover_carona.remover_carona(caronas, usuario)
             
             elif opcao2 == '7':
                 mostrar.detalhes_carona.detalhes(usuarios, caronas, passageiros)
 
             elif opcao2 == "8":
-                mostrar.carona_cadastrada.cadastradas_usuario(caronas, passageiros, usuario_logado)
+                mostrar.carona_cadastrada.cadastradas_usuario(caronas, passageiros, usuario)
 
             elif opcao2 == '9':
-                mostrar.relatorio.totalizadores(caronas, passageiros, usuario_logado)
+                mostrar.relatorio.totalizadores(caronas, passageiros, usuario)
 
             elif opcao2 == '10':
                 os.system('cls' if os.name == 'nt' else 'clear')

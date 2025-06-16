dd2366c04d275cb766ba039889fb3eb73c22c102
Adryhanmiguell <migueladryhan202@gmail.com>
2025-06-16 20:39:46 -0300

    Add files via upload

--- cancelar_remover/cancelar_reserva.py
+++ cancelar_remover/cancelar_reserva.py
@@ -1,15 +1,9 @@
-def remover_reserva(caronas, usuarios):
-    email_motorista = input("\nDigite o email do motorista: ")
-    data_carona = input("Digite a data da carona (DD/MM/AAAA): ")
-    
-    for car in caronas:
-        for usu in usuarios:
-            if car['data'] == data_carona and usu['email'] == email_motorista:
-                for r in car['reservas']:
-                    car['vagas'] += r['vagas_reservadas']
-                    car['reservas'].remove(r)
-                    print("Reserva cancelada com sucesso")
-                    break
-            else:
-                print("Nenhuma reserva não encontrada")
+def remover_reserva(caronas, passageiros):
+    data = input('Digite a data para remover (DD/MM/AAAA): ')
 
+    for car in caronas:
+        if data in car["data"]:
+            for pasg in passageiros:
+                car["vagas"] += int(pasg["passageiros"])
+                print('\nReversa cancelada.')
+                passageiros.remove(pasg)
